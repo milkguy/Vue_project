fccb9c3b941b1b6c521b06db19236c469e3bae1e
milkguy <26329510@qq.com>
2022-03-04 17:12:41 +0800

    完成数据报表功能开发

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -5,23 +5,68 @@
             <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
             <el-breadcrumb-item>数据统计</el-breadcrumb-item>
             <el-breadcrumb-item>数据报表</el-breadcrumb-item>
-        </el-breadcrumb>
+        </el-breadcrumb> 
         <!-- 卡片示图区 -->
         <el-card>
-            123
+            <div id="main" style="width: 600px;height:400px;"></div>
         </el-card>
     </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
+import _ from 'lodash'
 export default {
-    data(){
+    data(){ 
         return{
-
+            //需要合并的数据
+            options: {
+            title: {
+            text: '用户来源'
+            },
+            tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross',
+                label: {
+                backgroundColor: '#E9EEF3'
+                }
+            }
+            },
+            grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+            },
+            xAxis: [
+            {
+                boundaryGap: false
+            }
+            ],
+            yAxis: [
+            {
+                type: 'value'
+            }
+            ]
+         }
         }
     },
     created(){
 
+    },
+    //此时页面上的元素已经被渲染完毕
+    async mounted(){
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(document.getElementById('main'));
+      const {data:res} = await this.$http.get('reports/type/1')
+      if(res.meta.status!==200){
+          return this.$message.error('获取报表数据失败')
+      }
+
+      const result = _.merge(res.data,this.options)
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(result);
     },
     methods:{
 
